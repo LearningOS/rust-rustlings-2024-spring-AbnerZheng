aeace07edfe2d1dc9bcfc1448604189d5fcec6e6
Abner Zheng <abnerzheng@gmail.com>
2024-06-13 14:34:15 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(s: i32) -> i32 {
+    if s > 40 {
+        s
+    } else {
+        s * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
